8c6c5ffb3c94f71806cd6697b582c9ef9b794d3e
HawToo <78495152+HawToo@users.noreply.github.com>
2023-03-03 12:43:06 -0800

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,3 @@
 getgenv().APIKey = "Your API Key"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HawToo/spotify-rblx/main/spotxrblx.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HawToo/spotify-rblx/main/spotxrblx.lua"))()
